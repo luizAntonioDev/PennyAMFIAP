7e1fda34fafd75a02a39fa629ab5e9f8a90f52c7
Flávio Portugal Ramos da Conceição <rm79605@fiap.com.br>
2019-10-10 22:32:09 -0300

    Configurando Api

---
+++ PennyAM/PennyApi/Controllers/EmployController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PennyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployController : ControllerBase
+    {
+       
+    }
+}
